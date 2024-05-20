44bdcef52494412023aa56591a80fe4b86883be0
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-05-20 20:56:02 +0530

    Remove Direct Link

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose' );
 
 const connectDB = async () => {
     try{
-        const connect = await mongoose.connect("mongodb+srv://MALIK2004:MALIK2004@cluster0.jyqcmva.mongodb.net/SkillUpTechie");
+        const connect = await mongoose.connect(process.env.DATABASE);
         console.log("Database Connected: ",connect.connection.host, connect.connection.name);
     } catch(er) {
         console.log("Database Connection Failed");
